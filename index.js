5ada45ce5f3a09cb5de3efba04bf6232beae22be
shixiaoquan <1010083274@qq.com>
2018-07-05 17:11:33 +0800

    🐶: new work

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: Edmond.Shi 
+ * @Date: 2018-07-05 16:56:33 
+ * @Last Modified by: Edmond.Shi
+ * @Last Modified time: 2018-07-05 16:57:05
+ */
+const app = 'a good project';
+
+export default app;
